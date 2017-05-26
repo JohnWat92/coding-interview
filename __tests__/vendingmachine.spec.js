d4f641222a63b7e7f139155fb10a9d14d48515d7
Johnwat92 <JohnWat92@gmail.com>
2017-05-26 11:45:56 -0700

    made description and function

---
+++ __tests__/vendingmachine.spec.js
@@ -0,0 +1,9 @@
+const vendingMachine = require('../src/vendingMachine');
+
+describe('vendingMachine', () => {
+  describe('When printing out inventory', () => {
+    it('should return printed inventory', () => {
+
+    });
+  });
+});
---
+++ src/vendingMachine.js
@@ -0,0 +1,5 @@
+const vendingMachine = () => {
+
+}
+
+module.exports = vendingMachine;
